601e9b95f4019229efab7a5dbb8331192d9a765d
JonathanKayDev <67706563+JonathanKay86@users.noreply.github.com>
2022-03-23 21:20:00 +0000

    Add Postgres Db
    Add Postgres Db and update login partial

--- BugTracker/Program.cs
+++ BugTracker/Program.cs
@@ -8,7 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
