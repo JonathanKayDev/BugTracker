2b50c4f59d6a4caa19202ec768e681715a4eb506
JonathanKayDev <67706563+JonathanKay86@users.noreply.github.com>
2022-03-26 10:28:36 +0000

    Update TicketAttachment.cs

--- BugTracker/Models/TicketAttachment.cs
+++ BugTracker/Models/TicketAttachment.cs
@@ -17,6 +17,9 @@ namespace BugTracker.Models
         [DisplayName("Team Member")]
         public string? UserId { get; set; }
 
+        [DisplayName("File Description")]
+        public string? Description { get; set; }
+
         [NotMapped]
         [DataType(DataType.Upload)]
         public IFormFile? FormFile { get; set; }
