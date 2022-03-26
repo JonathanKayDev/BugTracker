13c1d18e1abd12d98e3a0e135cf6789460d3473a
JonathanKayDev <67706563+JonathanKay86@users.noreply.github.com>
2022-03-26 10:25:41 +0000

    Create TicketAttachment.cs

---
+++ BugTracker/Models/TicketAttachment.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BugTracker.Models
+{
+    public class TicketAttachment
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Ticket")]
+        public int TicketId { get; set; } // foreign key
+
+        [DisplayName("File Date")]
+        public DateTimeOffset Created { get; set; }
+
+        [DisplayName("Team Member")]
+        public string? UserId { get; set; }
+
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile? FormFile { get; set; }
+
+        [DisplayName("File Name")]
+        public string? FileName { get; set; }
+
+        public byte[]? FileData { get; set; }
+
+        [DisplayName("File Extension")]
+        public string? FileType { get; set; }
+
+
+
+        // Navigation properties
+        public virtual Ticket? Ticket { get; set; }
+        public virtual BTUser? User { get; set; }
+    }
+}
