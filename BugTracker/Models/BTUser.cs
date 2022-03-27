2b1334e8e721c78fbc698748fe81b5f84f070b2e
JonathanKayDev <67706563+JonathanKay86@users.noreply.github.com>
2022-03-27 21:05:54 +0100

    Update BTUser.cs

--- BugTracker/Models/BTUser.cs
+++ BugTracker/Models/BTUser.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -17,6 +18,26 @@ namespace BugTracker.Models
         [NotMapped]
         [Display(Name = "Full Name")]
         public string? FullName { get { return $"{FirstName} {LastName}"; } }
+
+        // Avatar
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile? AvatarFormFile { get; set; }
+
+        [DisplayName("Avatar")]
+        public string? AvatarFileName { get; set; }
+
+        public byte[]? AvatarFileData { get; set; }
+
+        [DisplayName("File Extension")]
+        public string? AvatarFileType { get; set; }
+
+        // Foreign Key
+        public int? CompanyId { get; set; }
+
+
+        // Navigation properties
+        public virtual ICollection<Project>? Projects { get; set; } = new HashSet<Project>();
 
     }
 }
