a187eb86038daf9e385707f5624844fed9ad2b7d
JonathanKayDev <67706563+JonathanKay86@users.noreply.github.com>
2022-03-30 20:00:00 +0100

    Create IBTRolesService.cs

---
+++ BugTracker/Services/Interfaces/IBTRolesService.cs
@@ -0,0 +1,16 @@
+﻿using BugTracker.Models;
+
+namespace BugTracker.Services.Interfaces
+{
+    public interface IBTRolesService
+    {
+        public Task<bool> IsUserInRoleAsync(BTUser user, string roleName);
+        public Task<IEnumerable<string>> GetUserRolesAsync(BTUser user);
+        public Task<bool> AddUserToToleAsync(BTUser user, string roleName);
+        public Task<bool> RemoveUserFromRoleAsync(BTUser user, string roleName);
+        public Task<bool> RemoveUserFromRolesAsync(BTUser user, IEnumerable<string> roles);
+        public Task<List<BTUser>> GetUsersInRoleAsync(string roleName, int companyId);
+        public Task<List<BTUser>> GetUsersNotInRoleAsync(string roleName, int companyId);
+        public Task<string> GetRoleNameByIdAsync(string roleId);
+    }
+}
