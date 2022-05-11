07198a3fd8aad20963e6a98bfb629f00a00a20a3
JonathanKayDev <67706563+JonathanKay86@users.noreply.github.com>
2022-05-11 20:53:46 +0100

    Create MyTickets controller function and view

--- BugTracker/Controllers/TicketsController.cs
+++ BugTracker/Controllers/TicketsController.cs
@@ -43,6 +43,15 @@ namespace BugTracker.Controllers
             return View(await applicationDbContext.ToListAsync());
         }
 
+        //GET: MyTickets
+        public async Task<IActionResult> MyTickets()
+        {
+            BTUser bTUser = await _userManager.GetUserAsync(User);
+            List<Ticket> tickets = await _ticketService.GetTicketsByUserIdAsync(bTUser.Id, bTUser.CompanyId);
+
+            return View(tickets);
+        }
+
         // GET: Tickets/Details/5
         public async Task<IActionResult> Details(int? id)
         {
