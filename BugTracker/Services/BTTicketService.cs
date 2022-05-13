96583bc693cb177df8d1e5df6c36ca83d6c33266
JonathanKayDev <67706563+JonathanKay86@users.noreply.github.com>
2022-05-13 20:18:07 +0100

    Update BTTicketService.cs
    Update GetTicketByIdAsync to include more ticket info: comments, attachments, & history

--- BugTracker/Services/BTTicketService.cs
+++ BugTracker/Services/BTTicketService.cs
@@ -330,6 +330,9 @@ namespace BugTracker.Services
                                         .Include(t => t.TicketPriority)
                                         .Include(t => t.TicketStatus)
                                         .Include(t => t.TicketType)
+                                        .Include(t => t.Comments)
+                                        .Include(t => t.Attachments)
+                                        .Include(t => t.History)
                                         .FirstOrDefaultAsync(t => t.Id == ticketId);
             }
             catch (Exception)
