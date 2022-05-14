789a9a01e7037bd444d5fe07ff90fd26725f9f77
JonathanKayDev <67706563+JonathanKay86@users.noreply.github.com>
2022-05-14 17:02:08 +0100

    Update TicketAttachment.cs

--- BugTracker/Models/TicketAttachment.cs
+++ BugTracker/Models/TicketAttachment.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using BugTracker.Extensions;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -17,7 +18,10 @@ namespace BugTracker.Models
         public string? Description { get; set; }
 
         [NotMapped]
+        [DisplayName("Select a file")]
         [DataType(DataType.Upload)]
+        [MaxFileSize(1024 * 1024)]
+        [AllowedExtensions(new string[] { ".jpg", ".png", ".doc", ".docx", ".xls", ".xlsx", ".pdf" })]
         public IFormFile? FormFile { get; set; }
 
         [DisplayName("File Name")]
