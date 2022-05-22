b92e5b39f774f207d67575c5fc2e554690bc2e7f
JonathanKayDev <67706563+JonathanKay86@users.noreply.github.com>
2022-05-22 09:08:53 +0100

    Add Plotly Bar chart to Dashboard

--- BugTracker/Controllers/HomeController.cs
+++ BugTracker/Controllers/HomeController.cs
@@ -11,24 +11,31 @@ namespace BugTracker.Controllers
 {
     public class HomeController : Controller
     {
+        #region Properties
         private readonly ILogger<HomeController> _logger;
         private readonly IBTCompanyInfoService _companyInfoService;
         private readonly IBTProjectService _projectService;
+        #endregion
 
+        #region Constructor
         public HomeController(ILogger<HomeController> logger,
-            IBTCompanyInfoService companyInfoService,
-            IBTProjectService projectService)
+                                IBTCompanyInfoService companyInfoService,
+                                IBTProjectService projectService)
         {
             _logger = logger;
             _companyInfoService = companyInfoService;
             _projectService = projectService;
         }
+        #endregion
 
+        #region Index
         public IActionResult Index()
         {
             return View();
         }
+        #endregion
 
+        #region Dashboard
         public async Task<IActionResult> Dashboard()
         {
             DashboardViewModel model = new();
@@ -36,25 +43,30 @@ namespace BugTracker.Controllers
 
             model.Company = await _companyInfoService.GetCompanyInfoByIdAsync(companyId);
             model.Projects = (await _companyInfoService.GetAllProjectsAsync(companyId)).Where(p => p.Archived == false).ToList();
-            model.Tickets = model.Projects.SelectMany(p=>p.Tickets).Where(t=>t.Archived == false).ToList();
+            model.Tickets = model.Projects.SelectMany(p => p.Tickets).Where(t => t.Archived == false).ToList();
             model.Members = model.Company.Members.ToList();
 
 
             return View(model);
         }
+        #endregion
 
+        #region Privacy
         public IActionResult Privacy()
         {
             return View();
         }
+        #endregion
 
+        #region Error
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        #endregion
 
-
+        #region Google Charts
         [HttpPost]
         public async Task<JsonResult> GglProjectTickets()
         {
@@ -93,7 +105,9 @@ namespace BugTracker.Controllers
 
             return Json(chartData);
         }
+        #endregion
 
+        #region AM Charts
         [HttpPost]
         public async Task<JsonResult> AmCharts()
         {
@@ -121,6 +135,45 @@ namespace BugTracker.Controllers
 
             return Json(amChartData.Data);
         }
+        #endregion
+
+        #region Plotly Charts
+        [HttpPost]
+        public async Task<JsonResult> PlotlyBarChart()
+        {
+            PlotlyBarData plotlyData = new();
+            List<PlotlyBar> barData = new();
+
+            int companyId = User.Identity.GetCompanyId().Value;
+
+            List<Project> projects = await _projectService.GetAllProjectsByCompany(companyId);
+
+            //Bar One
+            PlotlyBar barOne = new()
+            {
+                X = projects.Select(p => p.Name).ToArray(),
+                Y = projects.SelectMany(p => p.Tickets).GroupBy(t => t.ProjectId).Select(g => g.Count()).ToArray(),
+                Name = "Tickets",
+                Type = "bar"
+            };
+
+            //Bar Two
+            PlotlyBar barTwo = new()
+            {
+                X = projects.Select(p => p.Name).ToArray(),
+                Y = projects.Select(async p => (await _projectService.GetProjectMembersByRoleAsync(p.Id, nameof(Roles.Developer))).Count).Select(c => c.Result).ToArray(),
+                Name = "Developers",
+                Type = "bar"
+            };
+
+            barData.Add(barOne);
+            barData.Add(barTwo);
+
+            plotlyData.Data = barData;
+
+            return Json(plotlyData);
+        } 
+        #endregion
 
     }
 }
